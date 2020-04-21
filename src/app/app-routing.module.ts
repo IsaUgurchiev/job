4a7ecfd6eaf8eae1143cc266f093dccbf8204f26
Isa Ugurchiev <iugurchiev@iponweb.net>
2020-04-21 18:50:10 +0300

    Created CV page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ProfileComponent} from './pages/profile/profile.component';
+import {CvComponent} from './pages/profile/cv/cv.component';
+import {MainComponent} from './pages/main/main.component';
+
+const profileRoutes: Routes = [
+  {path: '', redirectTo: 'cv', pathMatch: 'prefix'},
+  {path: 'cv', component: CvComponent}
+];
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'profile', component: ProfileComponent, children: profileRoutes}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { CvComponent } from './pages/profile/cv/cv.component';
+import { MainComponent } from './pages/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    CvComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
